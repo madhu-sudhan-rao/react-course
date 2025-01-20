641da440f89f837b86a0210377dcd5cd738bab60
Madhu Sudhan Rao <madhusudanrao.pediredla@imaginnovate.com>
2025-01-20 15:43:15 +0530

    custom react

--- 01basic-vite/src/main.jsx
+++ 01basic-vite/src/main.jsx
@@ -1,9 +1,34 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+
+const reactElement = {
+  type: 'a',
+  props: {
+      href: 'https://google.com',
+      target: '_blank'
+  },
+  children: 'Click me to visit google'
+}
+
+function MyApp() {
+  return (
+    <div><h1>Custom React App</h1></div>
+  )
+}
+
+const AnotherElement = (
+  <a href="https://google.com">Visit Google</a>
+)
+
+const aReactElement = React.createElement(
+  'a',
+  {href: reactElement.props.href, target: reactElement.props.target,
+  },
+  'Visit Google 2'
+)
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  aReactElement
 )
