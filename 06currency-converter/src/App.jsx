d3f13148f54e50135913fce8eb1de2ebb3ade703
Madhu Sudhan Rao <madhusudanrao.pediredla@imaginnovate.com>
2025-02-03 14:50:48 +0530

    currency converter project initiated

---
+++ 06currency-converter/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+          <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+            Hey
+        </div>
+      
+    </>
+  )
+}
+
+export default App
