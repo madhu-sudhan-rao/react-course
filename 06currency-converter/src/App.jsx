6ca12e92426cb37f57b2a73638e69954cce67ce0
Madhu Sudhan Rao <madhusudanrao.pediredla@imaginnovate.com>
2025-04-08 10:20:17 +0530

    cc

--- 06currency-converter/src/App.jsx
+++ 06currency-converter/src/App.jsx
@@ -1,13 +1,21 @@
 
 import './App.css'
+import InputBox from './components/InputBox';
+
+
 
 function App() {
 
+  const backgroundImageUrl = 'https://static.vecteezy.com/system/resources/thumbnails/055/781/145/small_2x/fire-color-in-the-white-smoke-stream-on-a-solid-black-background-free-video.jpg';
+
   return (
     <>
-          <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-            Hey
-        </div>
+         <div className='flex justify-center items-center h-screen w-full flex-wrap bg-cover bg-no-repeat'
+          style={{backgroundImage: `url(${backgroundImageUrl})`}}
+         >
+            <h1 className='text-4xl font-bold text-white'>Currency Converter</h1>
+            <InputBox/>
+         </div>
       
     </>
   )
